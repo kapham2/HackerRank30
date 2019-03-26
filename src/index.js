b0c84dce41998dd248e0924c286a6e1659721483
Kim Pham <pham.kim.a@gmail.com>
2019-03-26 14:13:18 -0500

    Day 4: Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Day 4: Class vs. Instance
+    function Person(initialAge) {
+        let age;
+
+        if (initialAge < 0) {
+            console.log("Age is not valid, setting age to 0.")
+            age = 0;
+        } else {
+            age = initialAge;
+        }
+
+        this.amIOld=function(){
+        // Do some computations in here and print out the correct statement to the console
+            if (age < 13) {
+                console.log("You are young.");
+            }
+            else if (age < 18) {
+                console.log("You are a teenager.");
+            }
+            else {
+                console.log("You are old.");
+            }
+
+        };
+        this.yearPasses=function(){
+        // Increment the age of the person in here
+            age++;
+        };
+    
+    }
+
+    graham = new Person(32)
+    graham.amIOld()
+    graham.yearPasses()
+    graham.yearPasses()
+    graham.yearPasses()
+    graham.amIOld()
+    
+    div4 = document.querySelector('div[id="4"]');
+    div4.innerHTML = "Day 4: Class vs Instance"
+    console.log(div4.innerHTML)
+    printLine()
+
     // Day 3: Intro to Conditional Statements
     function main3() {
         const N = 3
